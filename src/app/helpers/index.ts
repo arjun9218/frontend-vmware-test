2e2800d4367d7a9d4c04a8a0c2ba6637da7a4308
Arjun G <nag9209@gmail.com>
2020-07-26 14:50:01 +0530

    Hosting to github pages

--- src/app/helpers/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './auth.helper';
-export * from './error.interceptor';
-export * from './token.interceptor';
-export * from './mock.backend';
--- src/app/models/user.ts
+++
@@ -1,8 +0,0 @@
-export class User {
-    emailId: string;
-    password: string;
-    firstName: string;
-    lastName: string;
-    token: string;
-    admin: boolean;
-}
